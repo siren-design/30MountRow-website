b71be920eea613d8f30e08ef2adff47e55d0103c
coha306 <30989135+coha306@users.noreply.github.com>
2022-06-20 07:45:52 +0100

    final changes + src

---
+++ src/build/webpack.base.conf.js
@@ -0,0 +1,192 @@
+/* Base config:
+  ========================================================================== */
+
+const path = require('path')
+const glob = require('glob')
+const fs = require('fs')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+// const PurgecssPlugin = require('purgecss-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+
+// Main const
+const PATHS = {
+    src: path.join(__dirname, '../src'),
+    dist: path.join(__dirname, '../dist'),
+    assets: 'assets/'
+}
+
+// Pages const for HtmlWebpackPlugin
+// see more: https://github.com/vedees/webpack-template/blob/master/README.md#html-dir-folder
+const PAGES_DIR = PATHS.src
+const PAGES = fs
+    .readdirSync(PAGES_DIR)
+    .filter(fileName => fileName.endsWith('.html'))
+
+module.exports = {
+    externals: {
+        paths: PATHS
+    },
+    entry: {
+        app: PATHS.src
+            // module: `${PATHS.src}/your-module.js`,
+    },
+    output: {
+        // filename: `${PATHS.assets}js/[name].[contenthash].js`,
+        filename: `${PATHS.assets}js/[name].js`,
+        path: PATHS.dist,
+        /*
+          publicPath: '/' - relative path for dist folder (js,css etc)
+          publicPath: './' (dot before /) - absolute path for dist folder (js,css etc)
+        */
+        publicPath: '/'
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    name: 'vendors',
+                    test: /node_modules/,
+                    chunks: 'all',
+                    enforce: true
+                }
+            }
+        }
+    },
+    module: {
+        rules: [{
+                // JavaScript
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                // Vue
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loader: {
+                        scss: 'vue-style-loader!css-loader!sass-loader'
+                    }
+                }
+            },
+            {
+                // Fonts
+                test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: `${PATHS.assets}fonts`
+                }
+            },
+            {
+                // images / icons
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: `${PATHS.assets}img`
+                }
+            },
+            {
+                // scss
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                            config: { path: `./postcss.config.js` }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true }
+                    }
+                ]
+            },
+            {
+                // css
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                            config: { path: `./postcss.config.js` }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            '~': PATHS.src, // Example: import Dog from "~/assets/img/dog.jpg"
+            '@': `${PATHS.src}/js`, // Example: import Sort from "@/utils/sort.js"
+            vue$: 'vue/dist/vue.js'
+        }
+    },
+    plugins: [
+        // Vue loader
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            // filename: `${PATHS.assets}css/[name].[contenthash].css`
+            filename: `${PATHS.assets}css/[name].css`
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                // Images:
+                {
+                    from: `${PATHS.src}/${PATHS.assets}img`,
+                    to: `${PATHS.assets}img`
+                },
+                // Fonts:
+                {
+                    from: `${PATHS.src}/${PATHS.assets}fonts`,
+                    to: `${PATHS.assets}fonts`
+                },
+                // Static (copy to '/'):
+                {
+                    from: `${PATHS.src}/static`,
+                    to: ''
+                }
+            ]
+        }),
+        // new PurgecssPlugin({
+        //     paths: glob.sync(`${PATHS.src}/**/*`,  { nodir: true }),
+        //     // safelist: [/^swiper/]
+        //     safelist: {
+        //       greedy: [/fade$/]
+        //     }
+        // }),
+
+        /*
+          Automatic creation any html pages (Don't forget to RERUN dev server!)
+          See more:
+          https://github.com/vedees/webpack-template/blob/master/README.md#create-another-html-files
+          Best way to create pages:
+          https://github.com/vedees/webpack-template/blob/master/README.md#third-method-best
+        */
+        ...PAGES.map(
+            page =>
+            new HtmlWebpackPlugin({
+                template: `${PAGES_DIR}/${page}`,
+                filename: `./${page}`
+            })
+        )
+    ]
+}
---
+++ src/src/index.js
@@ -0,0 +1,21 @@
+// JS
+import './js/'
+
+// SCSS
+import './assets/scss/app.scss'
+
+// CSS (example)
+// import './assets/css/main.css'
+
+// Vue.js
+window.Vue = require('vue')
+
+// Vue components (for use in html)
+Vue.component('example-component', require('./js/components/Example.vue').default)
+
+// Vue init
+const app = new Vue({
+  el: '#app'
+})
+
+
